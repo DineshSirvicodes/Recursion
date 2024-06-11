11bca0894c142704df869358915769fc79dcfcb4
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-06-11 21:37:24 +0530

    Create Find-N-Factorial.cpp

---
+++ Find-N-Factorial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+// factorial
+int func(int n){
+   if(n == 0 || n == 1){
+        return 1;
+   }
+   return n * func(n-1);
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    cout << func(n);
+}
